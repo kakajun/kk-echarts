f54be482fea98597181efb52f69f0f1c896c4c78
马军 <253495832@qq.com>
2022-05-08 19:03:27 +0800

    fix: 2

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import copy from 'rollup-plugin-cpy'
-// import { loadEnv } from "vite";
-// const CWD = process.cwd();
-// import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 import VitePluginElementPlus from 'vite-plugin-element-plus'
 const prefix = 'monaco-editor/esm/vs'
-// 环境变量
-// const BASE_ENV_CONFIG = loadEnv("", CWD);
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
@@ -17,14 +12,8 @@ export default defineConfig(({ command, mode }) => {
       vue(),
       //由于cdn不会被打包进dist目录,所以这里特殊copy一下,为什么cdn不放到pulic里面呢?因为我lib和option都不需要cdn
       copy([{ files: 'cdn', dest: './dist/cdn' }]),
-      // monacoEditorPlugin({
-      //   publicPath: 'https://unpkg.com/vite-plugin-monaco-editor@1.0.5/cdn'
-      // }),
       VitePluginElementPlus({
         // 如果你需要使用 [component name].scss 源文件，你需要把下面的注释取消掉。
-        // 对于所有的 API 你可以参考 https://github.com/element-plus/vite-plugin-element-plus
-        // 的文档注释
-        // useSource: true
         format: mode === 'development' ? 'esm' : 'cjs'
       })
     ],
